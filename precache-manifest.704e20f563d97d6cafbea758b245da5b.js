67715a9289fbf689f96ec4a5f90ee8d8d53c7ddb
Rafal Moneta <rafalmoneta5@gmail.com>
2020-02-17 20:59:16 +0100

    Updates

---
+++ precache-manifest.704e20f563d97d6cafbea758b245da5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652f10aeb1ab9b2e61df3d5e4897c239",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "16efdfa6f163958606ef",
+    "url": "/movie-app-react/static/css/main.c66a8aae.chunk.css"
+  },
+  {
+    "revision": "f5b85ed68d8560f013ee",
+    "url": "/movie-app-react/static/js/2.2f7a88b6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-app-react/static/js/2.2f7a88b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16efdfa6f163958606ef",
+    "url": "/movie-app-react/static/js/main.a516ec1d.chunk.js"
+  },
+  {
+    "revision": "86e59928444f4abc1ed3",
+    "url": "/movie-app-react/static/js/runtime-main.adbca76e.js"
+  },
+  {
+    "revision": "0f4a7f7476ed84b3527c3f993de82a9c",
+    "url": "/movie-app-react/static/media/moviedbicon.0f4a7f74.svg"
+  }
+]);
